be061038634677f82190406896bc3350f8f59615
bigpandabusiness <130111432+bigpandabusiness@users.noreply.github.com>
2024-03-22 00:50:52 +0000

    Everything Ive done, I think

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 function addBox() {
-  const para = document.createElement("div");
-  const node = document.createTextNode("This is new.");
-  para.appendChild(node);
-  const element = document.getElementById("div1");
-  element.appendChild(para);
+  div = 
+  document.createElement('div')
+  div.classList.add('box')
+  // Create a text input element
+  var input = document.createElement("input")
+
+  // Set input type to text
+  input.type = "text"
+
+  // Set some initial value (optional)
+  input.value = "Edit me!"
+
+  // Set styles (optional)
+  input.style.width = "140px"
+  input.style.height = "10px"
+  input.style.border = "none"
+
+  //To get rid of "Edit me" on click
+  input.addEventListener("focus", function() {
+    if (input.value === "Edit me!") {
+      input.value = ""
+    }
+  })
+  
+  div.appendChild(input);
+  const element = 
+  document.getElementById("div1");
+  element.appendChild(div);
 }
+
