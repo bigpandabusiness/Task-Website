52c22e218c9909f15cfda25ba1c621441f71388f
bigpandabusiness <130111432+bigpandabusiness@users.noreply.github.com>
2024-03-26 02:27:24 +0000

    Added sidebar

--- js/script.js
+++ js/script.js
@@ -1,31 +1,73 @@
-function addBox() {
-  div = 
-  document.createElement('div')
-  div.classList.add('box')
-  // Create a text input element
-  var input = document.createElement("input")
 
-  // Set input type to text
-  input.type = "text"
 
-  // Set some initial value (optional)
-  input.value = "Edit me!"
+//Sign Up Function
+function signUp() {
+  // Get the values of the input fields
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
 
-  // Set styles (optional)
-  input.style.width = "140px"
-  input.style.height = "10px"
-  input.style.border = "none"
+  // Check if the passwords match
+  if (password === confirmPassword) {
+    // Create a new user object
+    var user = {
+      username: username,
+      password: password
+    };
 
-  //To get rid of "Edit me" on click
-  input.addEventListener("focus", function() {
-    if (input.value === "Edit me!") {
-      input.value = ""
-    }
-  })
-  
-  div.appendChild(input);
-  const element = 
-  document.getElementById("div1");
-  element.appendChild(div);
+    // Save the user object to localStorage
+    localStorage.setItem("user", JSON.stringify(user));
+
+    // Redirect the user to the home page
+    window.location.href = "index.html";
+  } else {
+    // Display an error message
+    alert("Passwords do not match. Please try again.");
+  }
 }
 
+//Login Function
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "admin" && password == "admin") {
+    window.location.href = "index.html";
+  } else {
+    alert("Invalid username or password");
+  }
+}
+
+// Sidebar Function
+function sideBar() {
+  const sidebarElement = document.getElementById("sidebar");
+  if (sidebarElement.innerHTML.trim() === "") {
+    // Sidebar content is empty, add menu button and other content
+    sidebarElement.innerHTML = `
+    <div class="sidenav" id="sidenav">
+      <span class="material-symbols-outlined" style="font-size: 30px;" onclick="sideBar()">
+        menu
+      </span>
+      <a href='#'>About</a>
+      <a href='#'>Services</a>
+      <a href='#'>Clients</a>
+      <a href='#'>Contact</a>
+      <a href='#'>About</a>
+      <a href='#'>Services</a>
+      <a href='#'>Clients</a>
+      <a href='#'>Contact</a>
+      <a href='#'>About</a>
+      <a href='#'>Services</a>
+      </div>`;
+  } else {
+    // Sidebar content exists, remove it
+    sidebarElement.innerHTML = "";
+  }
+}
+
+
+// var level = 1
+// var maximumXP = 5
+// var currentXP = 1
+// var xpBar = document.getElementById("xpBar")
+
+// function levelUp()
